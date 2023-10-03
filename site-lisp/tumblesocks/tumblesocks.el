461cd91cb20a9e65b9e4b28e5e44c15a83fc1941
Benjamin Gleitzman <gleitz@mit.edu>
2023-10-02 18:29:51 -0700

    add submodules as regular checked in files

---
+++ site-lisp/tumblesocks/tumblesocks.el
@@ -0,0 +1,18 @@
+;;; tumblesocks.el --- An Emacs tumblr client.
+
+;; Copyright 2012 gcr
+
+;; Author: gcr <gcr@sneakygcr.net>
+;; URL: http://github.com/gcr/tumblesocks
+;; License: zlib
+
+(defgroup tumblesocks nil
+  "Emacs tumblr client"
+  :group 'applications)
+
+(require 'tumblesocks-api)
+(require 'tumblesocks-user)
+(require 'tumblesocks-compose)
+(require 'tumblesocks-view)
+
+(provide 'tumblesocks)
