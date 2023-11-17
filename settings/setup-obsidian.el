54d905d3a055fb20a2e804c93d03aa70813ecb63
Benjamin Gleitzman <gleitz@mit.edu>
2023-11-17 08:17:19 -0800

    add new shortcuts for creating obsidian files

--- settings/setup-obsidian.el
+++ settings/setup-obsidian.el
@@ -2,6 +2,7 @@
 (obsidian-specify-path "~/Dropbox/Personal/documents/obsidian")
 ;; If you want a different directory of `obsidian-capture':
 (setq obsidian-inbox-directory "Gleitzvault")
+(setq obsidian-daily-notes-directory "Gleitzvault/notes")
 
 ;; Define obsidian-mode bindings
 (add-hook
@@ -23,5 +24,41 @@
 
 ;; Activate detection of Obsidian vault
 (global-obsidian-mode t)
+
+(defun obsidian-capture-with-title ()
+  "Create new obsidian daily note with a specific title.
+Fill in the buffer with a proper Markdown heading based on the name.
+In the `obsidian-daily-notes-directory' if set otherwise in `obsidian-inbox-directory' - if that's also unset,
+in `obsidian-directory' root.
+."
+  (interactive)
+  (let* ((title (read-from-minibuffer "Title: "))
+         ;; create a full-title with date and title, replacing any spaces with dashes
+         (full-title (s-concat (format-time-string "%Y-%m-%d") "-" (s-replace " " "-" title)))
+         (filename (s-concat obsidian-directory "/" obsidian-inbox-directory "/" full-title ".md"))
+         (clean-filename (s-replace "//" "/" filename)))
+    (find-file (expand-file-name clean-filename) t)
+    ;; capitalize first letter of each word in title
+    (insert (format "# %s\n" (s-titleize title)))
+    (save-buffer)
+    (add-to-list 'obsidian-files-cache clean-filename)))
+
+(defun obsidian-daily-note-with-title ()
+  "Create new obsidian daily note with a specific title.
+Fill in the buffer with a proper Markdown heading based on the name.
+In the `obsidian-daily-notes-directory' if set otherwise in `obsidian-inbox-directory' - if that's also unset,
+in `obsidian-directory' root.
+."
+  (interactive)
+  (let* ((title (read-from-minibuffer "Title: "))
+         ;; create a full-title with date and title, replacing any spaces with dashes
+         (full-title (s-concat (format-time-string "%Y-%m-%d") "-" (s-replace " " "-" title)))
+         (filename (s-concat obsidian-directory "/" obsidian-daily-notes-directory "/" full-title ".md"))
+         (clean-filename (s-replace "//" "/" filename)))
+    (find-file (expand-file-name clean-filename) t)
+    ;; capitalize first letter of each word in title
+    (insert (format "# %s\n" (s-titleize title)))
+    (save-buffer)
+    (add-to-list 'obsidian-files-cache clean-filename)))
 
 (provide 'setup-obsidian)
